6bf506feb385f1551d146f053b6b312c93617f66
prxssh <prxssh@pm.me>
2025-07-25 21:31:04 +0530

    feat: implement bencoding marshaller and unmarshaller

---
+++ internal/bencode/marshaller.go
@@ -0,0 +1,85 @@
+package bencode
+
+import (
+	"fmt"
+	"io"
+	"sort"
+	"strconv"
+)
+
+type BencodeMarshaller struct {
+	w io.Writer
+}
+
+func NewBencodeMarshaller(w io.Writer) *BencodeMarshaller {
+	return &BencodeMarshaller{w: w}
+}
+
+func (m *BencodeMarshaller) Marshal(v any) error {
+	switch vt := v.(type) {
+	case int:
+		return m.marshalInteger(int64(vt))
+	case int64:
+		return m.marshalInteger(vt)
+	case string:
+		return m.marshalString(vt)
+	case []any:
+		return m.marshalList(vt)
+	case map[string]any:
+		return m.marshalDict(vt)
+	default:
+		return fmt.Errorf("bencode: unsupported type %T", vt)
+	}
+}
+
+/////////////// Private ///////////////
+
+func (m *BencodeMarshaller) marshalInteger(val int64) error {
+	_, err := m.w.Write([]byte("i" + strconv.FormatInt(val, 10)))
+	return err
+}
+
+func (m *BencodeMarshaller) marshalString(s string) error {
+	_, err := m.w.Write([]byte(strconv.Itoa(len(s)) + ":" + s))
+	return err
+}
+
+func (m *BencodeMarshaller) marshalList(list []any) error {
+	if _, err := m.w.Write([]byte("l")); err != nil {
+		return err
+	}
+
+	for _, item := range list {
+		if err := m.Marshal(item); err != nil {
+			return err
+		}
+	}
+
+	_, err := m.w.Write([]byte("e"))
+	return err
+}
+
+func (m *BencodeMarshaller) marshalDict(dict map[string]any) error {
+	if _, err := m.w.Write([]byte("d")); err != nil {
+		return err
+	}
+
+	// Bencode spec requires keys sorted lexicographically
+	keys := make([]string, 0, len(dict))
+	for k := range dict {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		if err := m.marshalString(k); err != nil {
+			return err
+		}
+		if err := m.Marshal(dict[k]); err != nil {
+			return err
+		}
+	}
+
+	_, err := m.w.Write([]byte("e"))
+	return err
+}
---
+++ internal/bencode/unmarshaller.go
@@ -0,0 +1,140 @@
+package bencode
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strconv"
+)
+
+type BencodeUnmarshaller struct {
+	r *bufio.Reader
+}
+
+type bencodedType byte
+
+const (
+	bInteger    bencodedType = 'i'
+	bDict       bencodedType = 'd'
+	bList       bencodedType = 'l'
+	bTerminator bencodedType = 'e'
+)
+
+func NewBencodeUnmarshaller(r io.Reader) *BencodeUnmarshaller {
+	return &BencodeUnmarshaller{r: bufio.NewReader(r)}
+}
+
+func (u *BencodeUnmarshaller) Unmarshal() (any, error) {
+	btype, err := u.r.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	switch btype {
+	case byte(bInteger):
+		return u.unmarshalInteger()
+	case byte(bDict):
+		return u.unmarshalDict()
+	case byte(bList):
+		return u.unmarshalList()
+	default:
+		if err := u.r.UnreadByte(); err != nil {
+			return nil, err
+		}
+		return u.unmarshalString()
+	}
+}
+
+/////////////// Private ///////////////
+
+func (u *BencodeUnmarshaller) unmarshalInteger() (int64, error) {
+	return u.readInteger(bTerminator)
+}
+
+func (u *BencodeUnmarshaller) unmarshalString() (string, error) {
+	size, err := u.readInteger(':')
+	if err != nil {
+		return "", err
+	}
+
+	if size == 0 {
+		return "", errors.New("bencode: empty string")
+	}
+
+	if size == 1 {
+		return "", errors.New(
+			"bencode: invalid string, negative length",
+		)
+	}
+
+	buf := make([]byte, size)
+	if _, err := io.ReadFull(u.r, buf); err != nil {
+		return "", err
+	}
+
+	return string(buf), nil
+}
+
+func (u *BencodeUnmarshaller) unmarshalList() ([]any, error) {
+	var list []any
+
+	for {
+		peek, err := u.r.Peek(1)
+		if err != nil {
+			return nil, err
+		}
+
+		if peek[0] == byte(bTerminator) {
+			u.r.ReadByte()
+			break
+		}
+
+		v, err := u.Unmarshal()
+		if err != nil {
+			return nil, err
+		}
+		list = append(list, v)
+	}
+
+	return list, nil
+}
+
+func (u *BencodeUnmarshaller) unmarshalDict() (map[string]any, error) {
+	dict := make(map[string]any)
+
+	for {
+		peek, err := u.r.Peek(1)
+		if err != nil {
+			return nil, err
+		}
+
+		if peek[0] == byte(bTerminator) {
+			u.r.ReadByte()
+			break
+		}
+
+		key, err := u.unmarshalString()
+		if err != nil {
+			return nil, err
+		}
+
+		val, err := u.Unmarshal()
+		if err != nil {
+			return nil, err
+		}
+
+		dict[string(key)] = val
+	}
+
+	return dict, nil
+}
+
+func (u *BencodeUnmarshaller) readInteger(delim bencodedType) (int64, error) {
+	read, err := u.r.ReadBytes(byte(delim))
+	if err != nil {
+		return -1, err
+	}
+
+	sint := string(read[:len(read)-1])
+	return strconv.ParseInt(sint, 10, 64)
+}
